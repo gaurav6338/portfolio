9c48c8d734efeb95953be96b141f45715e9df8b3
Gaurav <gauravrai01882002@gmail.com>
2026-09-03 22:39:28 +0530

    fix: Correct import path for useHeroParallax hook

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useHeroParallax } from "../hooks/useHeroParallax";
+import { useHeroParallax } from "../hooks/useheroparallax";
 
 gsap.registerPlugin(ScrollTrigger);
 
